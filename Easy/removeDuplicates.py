90f49a3b17c6e2a9563da0fa967dc06bc2247bec
Elad Asaf <Elad220@gmail.com>
2021-07-29 11:15:59 +0300

    done

--- Easy/removeDuplicates.py
+++ Easy/removeDuplicates.py
@@ -1,6 +1,13 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        for i in range(len(list)-1):
-            if list[i] == list[i+1]:
-                list[i].remove()
+    def removeDuplicates(self, nums):
+        dif_num=1
+        if nums==[]:
+            return 0
 
+        for i in range(len(nums)):
+            if(i+1<len(nums)):
+                if nums[i]<nums[i+1]:
+                    nums[dif_num]=nums[i+1]
+                    dif_num+=1     
+        print(nums)
+        return dif_num
